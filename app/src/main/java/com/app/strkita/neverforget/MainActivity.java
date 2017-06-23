0a0691730f8f1b70c6fd69c4f23018faa8c76e51
Hiroaki Kitada <kita83@gmail.com>
2017-06-23 22:26:44 +0900

    first commit

---
+++ app/src/main/java/com/app/strkita/neverforget/MainActivity.java
@@ -0,0 +1,85 @@
+package com.app.strkita.neverforget;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    final String[] fragments = {
+        "com.app.strkita.neverforget.MysizeFragment",
+            "com.app.strkita.neverforget.MemorialFragment",
+            "com.app.strkita.neverforget.PropertyFragment",
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        drawer.openDrawer(GravityCompat.START);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        switch (id) {
+            case R.id.nav_mysize:
+                fragmentManager.beginTransaction()
+                        .replace(R.id.container, Fragment.instantiate(MainActivity.this, fragments[0]))
+                        .commit();
+                break;
+            case R.id.nav_memorial:
+                fragmentManager.beginTransaction()
+                        .replace(R.id.container, Fragment.instantiate(MainActivity.this, fragments[1]))
+                        .commit();
+                break;
+            case R.id.nav_property:
+                fragmentManager.beginTransaction()
+                        .replace(R.id.container, Fragment.instantiate(MainActivity.this, fragments[2]))
+                        .commit();
+                break;
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
---
+++ app/src/main/java/com/app/strkita/neverforget/MysizeFragment.java
@@ -0,0 +1,104 @@
+package com.app.strkita.neverforget;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.ShareCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+
+public class MysizeFragment extends Fragment {
+
+    public MysizeFragment() {}
+
+    public static MysizeFragment newInstance(Context context) {
+        MysizeFragment f = new MysizeFragment();
+        return f;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ViewGroup root = (ViewGroup)inflater.inflate(R.layout.fragment_mysize, null);
+        return root;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        SharedPreferences prefs = this.getActivity().getSharedPreferences("mySize",
+                Context.MODE_PRIVATE);
+
+        int neck = prefs.getInt("neck", 0);
+        int sleeve = prefs.getInt("sleeve", 0);
+        int waist = prefs.getInt("waist", 0);
+        int insideLeg= prefs.getInt("insideLeg", 0);
+
+        EditText edText1 = (EditText)getView().findViewById(R.id.editText1);
+        if (neck != 0) edText1.setText(Integer.toString(neck));
+
+        EditText edText2 = (EditText)getView().findViewById(R.id.editText2);
+        if (sleeve != 0) edText2.setText(Integer.toString(sleeve));
+
+        EditText edText3 = (EditText)getView().findViewById(R.id.editText3);
+        if (waist != 0) edText3.setText(Integer.toString(waist));
+
+        EditText edText4 = (EditText)getView().findViewById(R.id.editText4);
+        if (insideLeg != 0) edText4.setText(Integer.toString(insideLeg));
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        EditText edText1 = (EditText)getView().findViewById(R.id.editText1);
+        EditText edText2 = (EditText)getView().findViewById(R.id.editText2);
+        EditText edText3 = (EditText)getView().findViewById(R.id.editText3);
+        EditText edText4 = (EditText)getView().findViewById(R.id.editText4);
+
+        int neck;
+        try {
+            neck = Integer.parseInt(edText1.getText().toString());
+        } catch (NumberFormatException e) {
+            neck = 0;
+        }
+
+        int sleeve;
+        try {
+            sleeve = Integer.parseInt(edText2.getText().toString());
+        } catch (NumberFormatException e) {
+            sleeve = 0;
+        }
+
+        int waist;
+        try {
+            waist = Integer.parseInt(edText3.getText().toString());
+        } catch (NumberFormatException e) {
+            waist = 0;
+        }
+
+        int insideLeg;
+        try {
+            insideLeg = Integer.parseInt(edText4.getText().toString());
+        } catch (NumberFormatException e) {
+            insideLeg = 0;
+        }
+
+        SharedPreferences prefs = this.getActivity().getSharedPreferences("mySize",
+                Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putInt("neck", neck);
+        editor.putInt("sleeve", sleeve);
+        editor.putInt("waist", waist);
+        editor.putInt("insideLeg", insideLeg);
+
+        // commitの非同期版
+        editor.apply();
+    }
+}
+
+
